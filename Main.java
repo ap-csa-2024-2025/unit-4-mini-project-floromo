300548dff694044aad99a686e7b5269a9dc09772
floromo <145050916+floromo@users.noreply.github.com>
2024-12-05 18:18:12 +0000

    Finished Aglorithm 1, starting Algorithm 2

--- Main.java
+++ Main.java
@@ -20,14 +20,36 @@ public class Main
 	// This method should implement Algorithm 1 and print all neccessary data
 	public static void algorithm1(String input)
 	{
-		// TODO: Put solution to Algorithm 1 here
-		System.out.println("Implement me!");
+		String lowerCase = input.toLowerCase();
+		String output = lowerCase.substring(0,1);
+		String vowels = "aeiou";
+		int vowelsCount = 0, repeatCount = 0, charSaved = 0;
+		for (int i = 1; i < lowerCase.length(); i++) {
+			String c = lowerCase.substring(i, i+1);
+			String cBefore = lowerCase.substring(i-1, i);
+			if (vowels.indexOf(c) != -1 && !cBefore.equals(" ")){
+				vowelsCount++;
+			} else if (c.equals(cBefore)){
+				repeatCount++;
+			} else {
+				output += c;
+			}
+
+		}
+		System.out.println("Algorithm 1\nVowels removed: " + vowelsCount + "\nRepeats removed: " + repeatCount + "\nAlgorithm 1 message: " + output + "\nAlgorithm 1 characters saved: " + (vowelsCount + repeatCount));
 	}
 
 	// This method should implement Algorithm 2 and print all neccessary data
 	public static void algorithm2(String input)
 	{
-		// TODO: Put solution to Algorithm 1 here
+		String unique = "";
+		for (int i = 1; i < input.length(); i++) {
+			String c = input.substring(i, i+1);
+			if (unique.indexOf(c) != -1) {
+
+			}
+
+		}
 		System.out.println("Implement me!");
 	}
 }
